5ea15477c72d95a0c9d2f2297dbe670182c23c13
Roy Mosby <ego.madking@gmail.com>
2018-01-07 14:56:09 -0500

    prototyped UI, ready for logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,10 @@
+$(function() {
+  $('.c-header__hamburger').click(function(){
+    $('.c-header__hamburger').toggleClass('c-header__hamburger--is-toggled');
+    $('.l-sidebar').toggleClass('l-sidebar--is-toggled');
+    $('.l-sidebar__search').toggleClass('l-sidebar__search--is-toggled');
+    $('.l-sidebar__results').toggleClass('l-sidebar__results--is-toggled');
+  });
 
+});
 
