a1e54025662d228f989fcc588deaf4061529d393
Roy Mosby <ego.madking@gmail.com>
2018-01-09 08:48:31 -0500

    WiP commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,46 @@
+
+/*
+Atlantic- a Neighborhood Eatery
+Green Truck Neighborhood Pub
+Cotton & Rye
+Sandfly BBQ
+Sandfly BBQ
+The Vault Kitchen & Market
+Chromatic Dragon
+Tequila's Town Mexican
+The Olde Pink House
+Flying Monk Noodle Bar
+
+
+Foxy Loxy Cafe
+Foundery Coffee Pub
+Sentient Bean
+Maté Factor
+The Coffee Fox
+Starbucks
+
+
+Leopold's Ice Cream
+Lulu's Chocolate Bar
+River Street Sweets
+Back in the Day Bakery
+*/
+
+
+
+function Model() {
+
+}
+
+function ViewModel () {
+  var self = this;
+
+}
+
+ko.applyBindings(new MapViewModel());
+
+
+// protyping function for menu. Dump into KO as appropriate
 $(function() {
   $('.c-header__hamburger').click(function(){
     $('.c-header__hamburger').toggleClass('c-header__hamburger--is-toggled');
@@ -7,4 +50,3 @@ $(function() {
   });
 
 });
-
