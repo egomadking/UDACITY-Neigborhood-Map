43dbf8e6151d123d38067bf596f2eecd40d4e800
Roy Mosby <ego.madking@gmail.com>
2018-01-12 10:14:19 -0500

    points object, skeleton of category and point classes

--- scripts/app.js
+++ scripts/app.js
@@ -1,45 +1,67 @@
-
-/*
-Atlantic- a Neighborhood Eatery
-Green Truck Neighborhood Pub
-Cotton & Rye
-Sandfly BBQ
-Sandfly BBQ
-The Vault Kitchen & Market
-Chromatic Dragon
-Tequila's Town Mexican
-The Olde Pink House
-Flying Monk Noodle Bar
-
-
-Foxy Loxy Cafe
-Foundery Coffee Pub
-Sentient Bean
-Maté Factor
-The Coffee Fox
-Starbucks
-
-
-Leopold's Ice Cream
-Lulu's Chocolate Bar
-River Street Sweets
-Back in the Day Bakery
-*/
-
-
+var points = {
+  'dining' : [
+    {'name': 'Cotton & Rye', 'address': '1801 Habersham St, Savannah, GA 31401'},
+    {'name': 'Sandfly BBQ', 'address': '8413 Ferguson Ave, Savannah, GA 31406'},
+    {'name': 'Chromatic Dragon', 'address': '514 Martin Luther King Jr Blvd, Savannah, GA 31401'},
+    {'name': "Tequila's Town Mexican", 'address': '109 Whitaker Street, Savannah, GA 31401'},
+    {'name': 'The Olde Pink House', 'address': '23 Abercorn St, Savannah, GA 31401'},
+    {'name': 'Flying Monk Noodle Bar', 'address': '5 W Broughton St, Savannah, GA 31401'},
+  ],
+  'café' : [
+    {'name': 'Foxy Loxy Cafe', 'address': '1919 Bull St, Savannah, GA 31401'},
+    {'name': 'Foundery Coffee Pub', 'address': '1313 Habersham St, Savannah, GA 31401'},
+    {'name': 'Sentient Bean', 'address': '13 E Park Ave, Savannah, GA 31401'},
+    {'name': 'Maté Factor', 'address': '401 E Hall St, Savannah, GA 31401'},
+    {'name': 'The Coffee Fox', 'address': '102 W Broughton St, Savannah, GA 31401'},
+  ],
+  'dessert' : [
+    {'name': "Leopold's Ice Cream", 'address': '212 E Broughton St, Savannah, GA 31401'},
+    {'name': "Lulu's Chocolate Bar", 'address': '42 Martin Luther King Jr Blvd, Savannah, GA 31401'},
+    {'name': 'River Street Sweets', 'address': '13 E River St, Savannah, GA 31401'},
+  ]
+};
 
 function Model() {
 
+  // Google geocoding
+
+  // Google map API calls
+
+  // Yelp reviews API calls
+
 }
 
-function ViewModel () {
+var Category = function(category) {
   var self = this;
-  this.isSidebarActive = ko.observable(false);
-  this.toggleSidebarActive = function(data, event) {
+
+};
+
+var Point = function(point) {
+  var self = this;
+  this.addToMap = function(){
+
+  };
+};
+
+var ViewModel = function() {
+  var self = this;
+  this.isSidebarActive = ko.observable(true);
+  this.toggleSidebarActive = function() {
     this.isSidebarActive(!this.isSidebarActive());
   };
-
-
-}
+// populate sidebar
+// call for initial Google Maps search of each point
+};
 
 ko.applyBindings(new ViewModel());
+
+
+/*
+ for each point I need to use:
+var marker = new google.maps.Marker({
+  position: myLatLng,
+  map: map,
+  title: 'Hello World!'
+});
+
+*/
