29e1d20d27bc86dea7ae7ba5b8ce979f68030731
Roy Mosby <ego.madking@gmail.com>
2018-01-15 14:55:35 -0500

    single ko.observable array. ko.computed populated list with filter.

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,14 @@
 // on ready jQuery wrapper
-// $( document ).ready(function() {
+//$( document ).ready(function() {
 
   // model
+
+  var map;
   var markers = [];
 
-  var geoFetch = function(address, name){
-    address = address + ' Savannah, GA';
+  var geoFetch = function(point){
+    var name = point.name;
+    address = point.address + ' Savannah, GA';
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode({'address': address}, function(results, status){
       if (status === 'OK') {
@@ -24,12 +27,23 @@
         });
 
         marker.addListener('click', function() {
-          map.setCenter(marker.getPosition());
+          //map.setCenter(marker.getPosition());
+          infowindowOver.close(map, marker);
           infowindowClick.open(map, marker);
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+          setTimeout(function(){ marker.setAnimation(null); }, 700);
         });
 
         marker.addListener('mouseover', function() {
-          infowindowOver.open(map, marker);
+          var test = infowindowClick.getMap();
+          // forces the mouseover infowindow to stay shut
+          // if the click-activated infowindow is active
+          if(test) {
+            return;
+            } else {
+              infowindowOver.open(map, marker);
+            }
+          
         });
 
         marker.addListener('mouseout', function() {
@@ -42,37 +56,47 @@
     });
   };
 
-    // Yelp reviews API calls
-
-
-  var addToMap = function(address, name) {
-      geoFetch(address, name);
-      //add binding for map crap
-  };
-    
+    // Yelp reviews API calls    
 
   var ViewModel = function() {
     var self = this;
-    self.staticDining = ko.observableArray([
+    self.staticPoints = ko.observableArray([
       {name: 'Cotton & Rye', address: '1801 Habersham St', type: 'dining'},
       {name: "Tequila's Town Mexican", address: '109 Whitaker Street', type: 'dining'},
-      {name: 'Flying Monk Noodle Bar', address: '5 W Broughton St', type: 'dining'}
-    ] // {name: 'val', address: 'val', type: 'val'}
-    );
-    self.staticCafe = ko.observableArray([
+      {name: 'Flying Monk Noodle Bar', address: '5 W Broughton St', type: 'dining'},
+      //café
       {name: 'Foxy Loxy Cafe', address: '1919 Bull St', type: 'café'},
       {name: 'Foundery Coffee Pub', address: '1313 Habersham St', type: 'café'},
-      {name: 'The Coffee Fox', address: '102 W Broughton St', type: 'café'}
-    ] // {name: 'val', address: 'val', type: 'val'}
-    );
-    self.staticDessert = ko.observableArray([
+      {name: 'The Coffee Fox', address: '102 W Broughton St', type: 'café'},
+      //dessert
       {name: "Leopold's Ice Cream", address: '212 E Broughton St', type: 'dessert'},
       {name: "Lulu's Chocolate Bar", address: '42 Martin Luther King Jr Blvd', type: 'dessert'},
       {name: 'River Street Sweets', address: '13 E River St', type: 'dessert'}
-    ] // {name: 'val', address: 'val', type: 'val'}
-    );
+    ]);
+    //observable array per category of POI
 
-    // style toggles
+    var hidingOldArraysBeacuseIamTiredofSpinningInCircles = function() {
+      self.staticDining = ko.observableArray([
+        {name: 'Cotton & Rye', address: '1801 Habersham St', type: 'dining'},
+        {name: "Tequila's Town Mexican", address: '109 Whitaker Street', type: 'dining'},
+        {name: 'Flying Monk Noodle Bar', address: '5 W Broughton St', type: 'dining'}
+      ] // {name: 'val', address: 'val', type: 'val'}
+      );
+      self.staticCafe = ko.observableArray([
+        {name: 'Foxy Loxy Cafe', address: '1919 Bull St', type: 'café'},
+        {name: 'Foundery Coffee Pub', address: '1313 Habersham St', type: 'café'},
+        {name: 'The Coffee Fox', address: '102 W Broughton St', type: 'café'}
+      ] // {name: 'val', address: 'val', type: 'val'}
+      );
+      self.staticDessert = ko.observableArray([
+        {name: "Leopold's Ice Cream", address: '212 E Broughton St', type: 'dessert'},
+        {name: "Lulu's Chocolate Bar", address: '42 Martin Luther King Jr Blvd', type: 'dessert'},
+        {name: 'River Street Sweets', address: '13 E River St', type: 'dessert'}
+      ] // {name: 'val', address: 'val', type: 'val'}
+      );
+  };
+
+    // filter toggles show/hide categories of POI
     self.showDining = ko.observable(true);
     self.toggleDiningFilter = function() {
       self.showDining(!self.showDining());
@@ -85,26 +109,40 @@
     self.toggleDessertFilter = function() {
       self.showDessert(!self.showDessert());
     };
+
+    //sidebar toggle for med, small screens
     self.isSidebarActive = ko.observable(true);
     self.toggleSidebarActive = function() {
       self.isSidebarActive(!self.isSidebarActive());
     };
 
-    // IIFE to map points in observable arrays
+    self.listItemsToShow = ko.computed(function() {
+      return ko.utils.arrayFilter(self.staticPoints(), function(point) {
+        if(self.showDining()){
+          if(point.type === 'dining'){
+            return point;
+          }
+        }if(self.showCafe()){
+          if(point.type === 'café'){
+            return point;
+          }
+        }if(self.showDessert()){
+          if(point.type === 'dessert'){
+            return point;
+          }
+        }
+      });
+    });
+
+    // IIFE to place POI on map
+    
     (function(){
-      for(var i = 0; i < self.staticDining().length; i++) {
-        addToMap(self.staticDining()[i].address, self.staticDining()[i].name);
-      }
-      for(var j = 0; j < self.staticCafe().length; j++) {
-        addToMap(self.staticCafe()[j].address, self.staticCafe()[j].name);
-      }
-      for(var k = 0; k < self.staticDessert().length; k++) {
-        addToMap(self.staticDessert()[k].address, self.staticDessert()[k].name);
+      for(var i = 0; i < self.staticPoints().length; i++){
+          geoFetch(self.staticPoints()[i]);
       }
     }(this));
-    };
-    
 
+    };
   ko.applyBindings(new ViewModel());
 
-// });
+//});
