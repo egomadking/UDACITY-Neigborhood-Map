234fffa279c269a2dd5082ee046f8c6941b0afed
Roy Mosby <egomadking@users.noreply.github.com>
2018-01-23 23:18:53 -0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -227,7 +227,7 @@ var fetchFourSqContent = function(marker) {
       rating = rating+'/10 with '+json.response.venue.ratingSignals+' ratings.';
       $('.gm-style-iw > h3').remove();
       $('.gm-style-iw').append('<div id="l-map__fourSq"></div>');
-      $('#l-map__fourSq').append('<img id=fourSqImg src="/assets/fourSq.png">');
+      $('#l-map__fourSq').append('<img id=fourSqImg src="https://raw.githubusercontent.com/egomadking/UDACITY-Neigborhood-Map/master/assets/fourSq.png">');
       $('#l-map__fourSq').append('<h2>'+rating+'</h2>');
       if(json.response.venue.hasMenu) {
         mobileUrl = json.response.venue.menu.mobileUrl;
